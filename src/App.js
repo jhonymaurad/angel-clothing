b31440900ea4fa83d7dba7ed7c792781ae9e89d0
jhony maurad <jhonymaurad@gmail.com>
2019-12-23 16:23:12 -0500

    added cartIcon and cartDropdown to our app, also implemented cart reducer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import './App.css';
-import Homepage from './pages/homepage/Homepage';
+
+import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-
-import { Route, Switch, Redirect } from 'react-router-dom';
-
-import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
 
-class App extends Component {
+class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
     const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
@@ -28,19 +28,21 @@ class App extends Component {
           });
         });
       }
-      setCurrentUser({ userAuth });
+
+      setCurrentUser(userAuth);
     });
   }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
+
   render() {
     return (
-      <div className="App">
+      <div>
         <Header />
         <Switch>
-          <Route exact path="/" component={Homepage} />
+          <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
           <Route
             exact
